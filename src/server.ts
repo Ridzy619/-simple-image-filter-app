01d9667a11165d0fcf8c72050bb41c8e2f449e51
Ridwan Salahuddeen <ridzy619@yahoo.com>
2022-07-22 22:07:32 +0100

    Add filtered image endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { requireAuth } from './auth';
 
 (async () => {
 
@@ -13,7 +14,27 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+  app.get("/filteredimage", requireAuth, async (req, res) => {
+    const image_url = req.query.image_url
 
+    if ( !image_url ){
+      return res.status(400).send({message: "Invalid image_url parameter provided. "+ image_url})
+    }
+
+    let filtered_image_path: string
+    try {
+    const filtered_image_path: string = await filterImageFromURL(image_url)
+    } catch(err) {
+      res.status(400).send({message: "Unable to process image: " + err})
+    }
+
+    await res.sendFile(filtered_image_path, (err) => {
+      res.status(400).send({message: "Error occured while returning the image."});
+    });
+  })
+  
+  // Root Endpoint
+  // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
